cfc8b8e2d0f07837e2ea3b10749fcf6c2f58675d
jbdenholtz <68787961+jbdenholtz@users.noreply.github.com>
2021-07-26 11:25:03 -0700

    stripe/sendgird/mongo added

--- index.php
+++ index.php
@@ -1,14 +1,57 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-use Cowsayphp\Farm;
 
-header('Content-Type: text/plain');
+echo "<h1>HELLO WORLD</>";
 
-$text = "Set a message by adding ?message=<message here> to the URL";
-if(isset($_GET['message']) && $_GET['message'] != '') {
-	$text = htmlspecialchars($_GET['message']);
-}
+echo "<div style='font-size: 16px;'>";
 
-$cow = Farm::create(\Cowsayphp\Farm\Cow::class);
-echo $cow->say($text);
+echo "<br><a href='php_info.php' target='_blank'>PHP INFO</a><br>";
+
+$db = mysqli_connect("test", "test", "test", "test");
+
+echo "<br><br>MYSQLI CONNECT<br>";
+
+print_r($db);
+
+$last_error = error_get_last();
+echo "<br>MYSQLI ERROR:<br>";
+print_r($last_error);
+
+//-----------------------------------------------------------------------------    
+
+echo "<br><br>STRIPE:<br>";
+$stripe = new \Stripe\StripeClient('123');
+print_r($stripe);
+
+echo "<br><br>Email:<br>";
+$email = new \SendGrid\Mail\Mail();
+print_r($email);
+
+echo "<br><br>MONGO EXTENSION LOADED:";
+echo extension_loaded("mongodb") ? "loaded\n" : "not loaded\n";
+
+
+echo "<br><br>MONGO MANAGER:<br>";
+$manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");    
+print_r($manager);
+
+echo "<br><br>MONGO QUERY:<br>";
+$query = new MongoDB\Driver\Query(array('age' => 30));    
+print_r($query);
+
+
+
+echo "<br><br>MONGO CLIENT:<br>";
+$client = new MongoDB\Client("mongodb://localhost:27017");    
+print_r($client);
+
+
+
+
+
+
+
+echo "</div>";
+
+?>
